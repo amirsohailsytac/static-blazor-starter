392b451f086ae9440dd8f8fbda9681e1b0c67fa0
amirsohailsytac <116566006+amirsohailsytac@users.noreply.github.com>
2025-03-14 12:35:15 +0100

    Update WeatherForecastFunction.cs

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -3,6 +3,7 @@ using BlazorApp.Shared;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Api
 {
